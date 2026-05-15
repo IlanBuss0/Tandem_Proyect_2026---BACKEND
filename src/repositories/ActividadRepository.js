a859c2e636fabcd619f6115d7eafe0a1f025f803
IlanBuss0 <ilanbusso7@gmail.com>
2026-05-15 08:18:00 -0300

    Expand CRUD endpoints for actividades and rutinas with filters and items

--- src/repositories/ActividadRepository.js
+++ src/repositories/ActividadRepository.js
@@ -1,8 +1,29 @@
 import BaseCrudRepository from './BaseCrudRepository.js';
+import BD from '../db/BD.js';
 
 class ActividadRepository extends BaseCrudRepository {
   constructor() {
     super('actividades');
+  }
+
+  findByPerteneciente(idPerteneciente) {
+    return BD.query('SELECT * FROM actividades WHERE id_perteneciente = $1 ORDER BY id DESC', [idPerteneciente]);
+  }
+
+  findBase() {
+    return BD.query('SELECT * FROM actividades WHERE (es_base = true OR tipo = $1) ORDER BY id DESC', ['base']);
+  }
+
+  findPersonalizadas() {
+    return BD.query('SELECT * FROM actividades WHERE (es_base = false OR tipo = $1) ORDER BY id DESC', ['personalizada']);
+  }
+
+  findByCategoria(categoria) {
+    return BD.query('SELECT * FROM actividades WHERE categoria = $1 ORDER BY id DESC', [categoria]);
+  }
+
+  findByTipo(tipo) {
+    return BD.query('SELECT * FROM actividades WHERE tipo = $1 ORDER BY id DESC', [tipo]);
   }
 }
 
